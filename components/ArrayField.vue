748f364fb0b3bf6b923521eeabbb0d6b712cd65c
kirimin <minkarakirimade@gmail.com>
2022-04-04 23:18:22 +0900

    created mockup

--- components/ArrayField.vue
+++ components/ArrayField.vue
@@ -1,39 +1,66 @@
 <template>
-  <v-container>
-    <v-col>
-      <h2>要素数</h2>
-      <v-row class="mt-8">
-        <h3>Height:</h3>
-        <v-text-field/>
-      </v-row>
+  <v-container >
+    <v-btn>表をコピー</v-btn>
+    <v-card class="mt-8 pa-4">
       <v-row>
         <h3>Width:</h3>
-        <v-text-field/>
+        <v-text-field v-model.number="x"/>
       </v-row>
-    </v-col>
-    <v-row
-      v-for="i in 10"
-      :key="i"
-      :class="i === 1 ? 'mb-6' : ''"
-      no-gutters
-    >
-      <v-col
-        v-for="j in 10"
-        :key="j"
+      <v-row class="mt-8">
+        <h3>Height:</h3>
+        <v-text-field v-model.number="y"/>
+      </v-row>
+      <v-row
+        v-for="i in y"
+        :key="i"
+        :class="i === 1 ? 'mb-6' : ''"
+        no-gutters
+        class="d-flex align-start mb-6"
       >
-        <v-card
-          outlined
-          tile
+        <v-col
+          v-for="j in x"
+          :key="j"
+          no-gutters
         >
-          <v-text-field />
-        </v-card>
-      </v-col>
-    </v-row>
+          <v-card
+            outlined
+          >
+            <v-card-text>({{ j - 1 }}, {{ i - 1 }})</v-card-text>
+            <v-text-field
+              class="number"
+              value="0"
+              justify="center"
+            />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
-<style>
-.nuxt-logo {
-  height: 180px;
+<script lang="ts">
+
+interface Data {
+  x: number
+  y: number
 }
+
+export default {
+
+  name: 'ArrayField',
+  data(): Data {
+    return {
+      y: 3,
+      x: 10,
+    }
+  },
+}
+</script>
+
+<style>
+.number input {
+  font-size: 1.5em;
+  text-align: center
+}
+
 </style>
